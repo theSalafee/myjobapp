be24ad81767023dad38c5888a7c183c47e965ffb
Naasir Salafee <naasirbush@icloud.com>
2022-04-09 07:51:31 -0700

    Initialize app and basic mockup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from 'react';
+import Table from './Table';
+class App extends Component {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    initialState = {
+        firstName: 'Naasir'
+    };
+
+    render() {
+        return (
+            <div className="container">
+                <h1>Hello, React!</h1>
+                <h2>{this.initialState.firstName}</h2>
+                <hr />
+                <Table />
+            </div>
+        )
+    }
 }
 
 export default App;
