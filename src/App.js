2b2cac4e260f661921b56083cb4a538ccc2c51cd
Naasir Salafee <naasirbush@icloud.com>
2022-04-09 08:25:13 -0700

    Prior to pulling in dynamic data

--- src/App.js
+++ src/App.js
@@ -2,17 +2,47 @@ import { Component } from 'react';
 import Table from './Table';
 class App extends Component {
 
-    initialState = {
-        firstName: 'Naasir'
+    state = {
+        firstName: 'Naasir',
+        characters: [
+            {
+                name: 'Charlie',
+                job: 'Janitor',
+            },
+            {
+                name: 'Mac',
+                job: 'Bouncer',
+            },
+            {
+                name: 'Dee',
+                job: 'Aspiring actress',
+            },
+            {
+                name: 'Dennis',
+                job: 'Bartender',
+            },
+        ],
     };
 
+    removeCharacter = (index) => {
+        const { characters } = this.state
+
+        this.setState({
+            characters: characters.filter((character, i) => {
+                return i !== index
+            }),
+        })
+    }
+
     render() {
+        const { characters } = this.state
+
         return (
-            <div className="container">
+            <div className="container" >
                 <h1>Hello, React!</h1>
-                <h2>{this.initialState.firstName}</h2>
+                <h2>{this.state.firstName}</h2>
                 <hr />
-                <Table />
+                <Table characterData={characters} removeCharacter={this.removeCharacter} />
             </div>
         )
     }
--- src/Table.js
+++ src/Table.js
@@ -9,37 +9,30 @@ const TableHeader = () => {
             </tr>
         </thead>
     );
-
 };
 
-const TableBody = () => {
-    return (
-        <tbody>
-            <tr>
-                <td>Charlie</td>
-                <td>Janitor</td>
+const TableBody = (props) => {
+    const rows = props.characterData.map((row, index) => {
+        return (
+            <tr key={index}>
+                <td>{row.name}</td>
+                <td>{row.job}</td>
+                <td>
+                    <button onClick={() => props.removeCharacter(index)}>Delete</button>
+                </td>
             </tr>
-            <tr>
-                <td>Mac</td>
-                <td>Bouncer</td>
-            </tr>
-            <tr>
-                <td>Dee</td>
-                <td>Aspiring actress</td>
-            </tr>
-            <tr>
-                <td>Dennis</td>
-                <td>Bartender</td>
-            </tr>
-        </tbody>
-    );
-};
+        )
+    })
 
-const Table = () => {
+    return <tbody>{rows}</tbody>
+}
+
+const Table = (props) => {
+    const { characterData, removeCharacter } = props;
     return (
         <table>
             <TableHeader />
-            <TableBody />
+            <TableBody characterData={characterData} removeCharacter={removeCharacter} />
         </table>
     );
 };
